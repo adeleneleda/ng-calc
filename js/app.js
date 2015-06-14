ca103cd53a303fa3b11795169011b9c53fe140d1
apfestin <festinadelen@gmail.com>
2015-06-14 22:12:08 +0800

    Fix alignment

--- js/app.js
+++ js/app.js
@@ -5,42 +5,42 @@ app.controller('calcuCtrl', ['$scope',
                              'MathService', 
                              'BasicOperationsServiceV2', 
   function($scope, BasicOperationService, MathService, BasicOperationsServiceV2) {
-  self = this;
+    self = this;
 
- 
-  $scope.add = function() {
-    $scope.answer = BasicOperationService.add($scope.firstNum, $scope.  secondNum);
-  };
+   
+    $scope.add = function() {
+      $scope.answer = BasicOperationService.add($scope.firstNum, $scope.  secondNum);
+    };
 
-  $scope.subtract = function() {
-    $scope.answer = BasicOperationService.subtract($scope.firstNum, $scope. secondNum);
-  };
+    $scope.subtract = function() {
+      $scope.answer = BasicOperationService.subtract($scope.firstNum, $scope. secondNum);
+    };
 
-  $scope.multiply = function() {
-    $scope.answer = BasicOperationService.multiply($scope.firstNum, $scope. secondNum);
-  };
+    $scope.multiply = function() {
+      $scope.answer = BasicOperationService.multiply($scope.firstNum, $scope. secondNum);
+    };
 
-  $scope.divide = function() {
-    $scope.answer = BasicOperationService.divide($scope.firstNum, $scope. secondNum);
-  };
+    $scope.divide = function() {
+      $scope.answer = BasicOperationService.divide($scope.firstNum, $scope. secondNum);
+    };
 
-  $scope.square = function() {
-    $scope.answer = MathService.square($scope.firstNum);
-  };
+    $scope.square = function() {
+      $scope.answer = MathService.square($scope.firstNum);
+    };
 
-  $scope.cube = function() {
-    $scope.answer = MathService.cube($scope.firstNum);
-  };
+    $scope.cube = function() {
+      $scope.answer = MathService.cube($scope.firstNum);
+    };
 
-  // To demonstrate factories:
+    // To demonstrate factories:
 
-  $scope.addv2 = function() {
-    $scope.answer = BasicOperationsServiceV2.add($scope.firstNum, $scope.  secondNum);
-  };
+    $scope.addv2 = function() {
+      $scope.answer = BasicOperationsServiceV2.add($scope.firstNum, $scope.  secondNum);
+    };
 
-  $scope.subtractv2 = function() {
-    $scope.answer = BasicOperationsServiceV2.subtract($scope.firstNum, $scope. secondNum);
-  };
+    $scope.subtractv2 = function() {
+      $scope.answer = BasicOperationsServiceV2.subtract($scope.firstNum, $scope. secondNum);
+    };
 
 }]);
 
